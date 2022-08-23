ea5e299b5d9b17aa58482807977ad852b97cefdb
MohammedNishadh <111855119+MohammedNishadh@users.noreply.github.com>
2022-08-23 13:30:00 +0530

    Add files via upload

---
+++ trail.php
@@ -0,0 +1,34 @@
+<?php
+$server_name="localhost";
+$username="root";
+$password="";
+$database_name="trail_database";
+$conn=mysqli_connect($server_name,$username,$password,$database_name);
+//now check the connection
+if(!$conn)
+{
+	die("Connection Failed:" . mysqli_connect_error());
+
+}
+if(isset($_POST['send']))
+{
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+
+    $sql_query = "INSERT INTO details (name,email,message)
+VALUES ('$name','$email','$message')";
+
+    if (mysqli_query($conn, $sql_query))
+    {
+		echo "New Details Entry inserted successfully !";
+    }
+    else
+    {
+		echo "Error: " . $sql . "" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
+
+?>
